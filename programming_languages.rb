ec68c323e71bf778c291190b0b843dab29c8838d
zahrank21 <zahrank@gmail.com>
2018-01-13 16:00:11 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,18 +1,18 @@
 def reformat_languages(languages)
   new_hash = {}
- languages.each do |style , lang|  
+ languages.each do |style , lang|
    lang.each do |lang2, type|
      new_hash[lang2] = {:type => type.values.first,
      :style => []
      }
-     
+
   if languages[style].keys.include?(lang2)
-    new_hash[lang2][:style] << style 
+    new_hash[lang2][:style] << style
   end
-  
-  
+
+
   end
-  
+
  end
 new_hash
 end
